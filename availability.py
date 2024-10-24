e46ee5ac08361838c29fbca2fc395dd1fb8410c1
relnaggar <ramsey.el-naggar@outlook.com>
2024-10-24 11:29:14 +0100

    add everything

---
+++ availability.py
@@ -0,0 +1,138 @@
+import datetime
+import json
+import pyperclip
+
+from outlook_calendar import OutlookCalendar, yesnoinput
+
+
+CACHE_PATH = "cache"
+EVENTS_PATH = f"{CACHE_PATH}/events.json"
+
+USER_PRINCIPAL_NAME = "ramsey.el-naggar@outlook.com"
+TARGET_CALENDAR_NAMES = ["Calendar", "Tutoring"]
+
+
+def create_cache_directory():
+  import os
+  if not os.path.exists(CACHE_PATH):
+    os.makedirs(CACHE_PATH)
+
+def get_cached_events():
+  try:
+    with open(EVENTS_PATH, "r") as f:
+      events = json.load(f)
+    return events
+  except FileNotFoundError:
+    pass
+  return None
+
+def cache_events(events):
+  with open(EVENTS_PATH, "w") as f:
+    json.dump(events, f, indent=2)
+
+def get_events(start_datetime, end_datetime):
+  outlook_calendar = OutlookCalendar(USER_PRINCIPAL_NAME, TARGET_CALENDAR_NAMES)
+  events = outlook_calendar.get_events(start_datetime, end_datetime)
+  cache_events(events)  
+  return events
+
+def get_ordinal_suffix(day):
+  if 4 <= day <= 20 or 24 <= day <= 30:
+    return "th"
+  else:
+    return ["st", "nd", "rd"][day % 10 - 1]
+
+def is_overlap(start_datetime_1, end_datetime_1, start_datetime_2, end_datetime_2):
+  latest_start = max(start_datetime_1, start_datetime_2)
+  earliest_end = min(end_datetime_1, end_datetime_2)
+  return latest_start < earliest_end
+
+def main():
+  create_cache_directory()
+
+  now = datetime.datetime.now()
+  end_of_next_week = (now + datetime.timedelta(days=(14 - now.isoweekday()))).replace(hour=23, minute=59, second=59)
+
+  start_datetime = now
+  end_datetime = end_of_next_week
+  events = get_cached_events()
+  if events is None or yesnoinput("Use cached events? ", default="n") == "n":
+    events = get_events(start_datetime, end_datetime)
+
+  for event in events:
+    event["startDateTime"] = datetime.datetime.fromisoformat(event["startDateTime"])
+    event["endDateTime"] = datetime.datetime.fromisoformat(event["endDateTime"])
+
+  workdays = list(range(1,4+1))
+  start_work_time = datetime.time(hour=12, minute=0)
+  end_work_time = datetime.time(hour=20, minute=0)
+
+  valid = False
+  while not valid:
+    meeting_type = input("Meeting type (1: 55-minute lesson, 2: 15-minute meeting): ")
+    if meeting_type == "1" or meeting_type == "2":
+      valid = True
+    else:
+      print("Invalid meeting type")
+  
+  if meeting_type == "1":
+    meeting_duration = datetime.timedelta(hours=1)
+    actual_meeting_duration = datetime.timedelta(minutes=55)
+  else:
+    meeting_duration = datetime.timedelta(minutes=30)
+    actual_meeting_duration = datetime.timedelta(minutes=15)
+
+  event_number = 0
+  today_date = datetime.date.today()
+  end_date = end_datetime.date()
+  current_date = today_date
+  availabile_hours = []
+  while current_date <= end_date:
+    if current_date.isoweekday() in workdays:
+      if current_date == today_date:
+        start_of_next_hour_datetime = (start_datetime.replace(minute=0, second=0, microsecond=0) + datetime.timedelta(hours=1))
+        start_hour_datetime = start_of_next_hour_datetime
+      else:
+        start_hour_datetime = datetime.datetime.combine(current_date, start_work_time)
+      current_hour_datetime = start_hour_datetime
+      # print("hour",current_hour_datetime)
+      while current_hour_datetime.time() < end_work_time and current_hour_datetime < end_datetime:
+        end_current_hour_datetime = current_hour_datetime+meeting_duration
+        if start_work_time <= current_hour_datetime.time() and end_current_hour_datetime.time() <= end_work_time:
+          while current_hour_datetime >= events[event_number]["endDateTime"] and event_number < len(events)-1:
+            event_number += 1
+            # print("event",events[event_number])
+          if not is_overlap(current_hour_datetime, end_current_hour_datetime, events[event_number]["startDateTime"], events[event_number]["endDateTime"]):
+            availabile_hours.append(current_hour_datetime)
+        current_hour_datetime += meeting_duration
+        # print("hour",current_hour_datetime)
+    current_date += datetime.timedelta(days=1)
+
+  availability_by_date = {}
+  for hour_datetime in availabile_hours:
+    date = hour_datetime.date()
+    if date in availability_by_date:
+      availability_by_date[date].append(hour_datetime)
+    else:
+      availability_by_date[date] = [hour_datetime]
+
+  availability = f"Current availability for a {int(actual_meeting_duration.total_seconds() // 60)}-minute session (UK local time):"
+  for date in sorted(availability_by_date.keys()):
+    availability += "\n"
+    availability += date.strftime(f"%A {date.day}{get_ordinal_suffix(date.day)} %B: ")
+    for hour_datetime in availability_by_date[date][:-1]:
+      availability += hour_datetime.strftime("%H:%M") + "-" + (hour_datetime+actual_meeting_duration).strftime("%H:%M") + ", "
+    hour_datetime = availability_by_date[date][-1]
+    availability += hour_datetime.strftime("%H:%M") + "-" + (hour_datetime+actual_meeting_duration).strftime("%H:%M")
+
+  print("")
+  print(availability)
+  print("")
+
+  pyperclip.copy(availability)
+  print("Copied to clipboard")
+
+
+if __name__ == "__main__":
+  main()
+
---
+++ outlook_calendar.py
@@ -0,0 +1,214 @@
+import json
+import time
+import requests
+import urllib.parse
+
+def yesnoinput(prompt, default=None):
+    valid = False
+    while valid == False:
+      user_input = input("(" + ("Y" if default == "y" else "y") + "/" + ("N" if default == "n" else "n") + ") " + prompt)
+      if user_input == "" and default is not None:
+        user_input = default
+      if user_input in ["y", "n"]:
+        valid = True
+      else:
+        print("invalid -- you must type 'y' or 'n'")
+    return user_input
+
+class OutlookCalendar:
+  CACHE_PATH = "cache"
+  ACCESS_TOKEN_PATH = f"{CACHE_PATH}/access_token.json"
+  CALENDARS_PATH = f"{CACHE_PATH}/calendars.json"
+
+  TENANT_ID = "consumers"
+  CLIENT_ID = open('secrets/CLIENT_ID').read().strip()
+  CLIENT_SECRET = open('secrets/CLIENT_SECRET').read().strip()
+  HOST = 'localhost'
+  PORT = 65432
+  REDIRECT_URI = f"http://{HOST}:{PORT}"
+  SCOPE = "calendars.read"
+
+  def __init__(self, user_principal_name, target_calendar_names):
+    self.user_principal_name = user_principal_name
+    self.target_calendar_names = target_calendar_names
+
+    self.access_token = self.get_cached_access_token()
+    if self.access_token is None or yesnoinput("Use cached access_token? ", default="y") == "n":
+      authorization_code = self.get_authorization_code()
+      self.access_token = self.get_access_token(authorization_code)
+
+    self.calendars = self.get_cached_calendars()
+    if self.calendars is None or yesnoinput("Use cached calendars? ", default="y") == "n":
+      self.calendars = self.get_calendars()
+
+  def get_cached_access_token(self):
+    try:
+      with open(self.ACCESS_TOKEN_PATH, "r") as f:
+        data = json.load(f)
+      now = time.time()
+      if data["expiry_time"] - now > 10:
+        return data["access_token"]
+    except FileNotFoundError:
+      pass
+    return None
+
+  def get_authorization_code(self):
+    import random
+    import pyperclip
+    import socket
+    
+    state = random.randint(100,1000)
+    authorize_url = f"https://login.microsoftonline.com/{self.TENANT_ID}/oauth2/v2.0/authorize?client_id={self.CLIENT_ID}&redirect_uri={urllib.parse.quote(self.REDIRECT_URI)}&scope={self.SCOPE}&response_type=code&response_mode=query&state={state}"
+
+    print("User authorization")
+    print("authorize_url:",authorize_url)
+    pyperclip.copy(authorize_url)
+    print("Copied to clipboard")
+    print("")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      s.bind((self.HOST, self.PORT))
+      s.listen()
+      print(f"Server listening on {self.HOST}:{self.PORT}")
+      conn, addr = s.accept()  
+      with conn:
+        print('Connected by', addr)
+        data = conn.recv(4096)
+        # print('Received', repr(data))
+        code = str(data).split("&")
+        assert len(code)==2
+        assert code[1][:len("state="+str(state))] == "state="+str(state)
+        code = code[0].split("code=")
+        assert len(code)==2
+        authorization_code = code[1]
+        print("Received authorization code")
+    print("")
+    return authorization_code
+
+  def cache_access_token(self, access_token, expiry_time):
+    cache = {
+      "access_token": access_token,
+      "expiry_time": expiry_time
+    }
+    with open(self.ACCESS_TOKEN_PATH, "w") as f:
+      json.dump(cache, f, indent=2)
+    print("Access token cached")
+
+  def get_access_token(self, authorization_code):
+    token_url = f"https://login.microsoftonline.com/{self.TENANT_ID}/oauth2/v2.0/token"
+    headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {
+      "client_id": self.CLIENT_ID,
+      "scope": self.SCOPE,
+      "grant_type": "authorization_code",
+      "code": authorization_code,
+      "redirect_uri": self.REDIRECT_URI,
+      "client_secret": self.CLIENT_SECRET
+    }
+    print("Requesting access token")
+    start_time = time.time()
+    response = requests.post(token_url, headers=headers, data=data, timeout=1)
+    print(response.status_code)
+    response_json = response.json()
+    # print('Received', response_json)
+    access_token = response_json["access_token"]
+    expiry_time = start_time + response_json["expires_in"]
+    print("Received access token")
+    # print(access_token)
+
+    self.cache_access_token(access_token, expiry_time)
+    return access_token
+
+  def get_cached_calendars(self):
+    try:
+      with open(self.CALENDARS_PATH, "r") as f:
+        calendars = json.load(f)
+      return calendars
+    except FileNotFoundError:
+      pass
+    return None
+
+  def cache_calendars(self, calendars):
+    with open(self.CALENDARS_PATH, "w") as f:
+      json.dump(calendars, f, indent=2)
+    print("Calendars cached")
+
+  def get_calendars(self):
+    headers = {
+        "Authorization": f"Bearer {self.access_token}",
+        "Content-Type": "application/json"
+    }
+
+    query_url = f"https://graph.microsoft.com/v1.0/users/{urllib.parse.quote(self.user_principal_name)}/calendars"
+    print("Requesting list of calendars")  
+    print("query_url:",query_url)
+    response = requests.get(query_url, headers=headers, timeout=1)
+    print(response.status_code)
+    print("")
+
+    data = response.json()
+    # print(json.dumps(data, indent=2))
+
+    calendars = []
+    for calendar in data["value"]:
+      if calendar["name"] in self.target_calendar_names:
+        print("appending calendar named",calendar["name"])
+        calendars.append({
+          "id": calendar["id"],
+          "name": calendar["name"]
+        })
+    print("")
+
+    self.cache_calendars(calendars)
+    return calendars
+
+  def get_calendar_view(self, calendar, startDateTime, endDateTime):
+    headers = {
+        "Authorization": f"Bearer {self.access_token}",
+        "Prefer": 'outlook.timezone="GMT Standard Time"'
+    }
+
+    params = {
+        "startDateTime": startDateTime.isoformat(),
+        "endDateTime": endDateTime.isoformat(),
+        "$orderby": "start/dateTime asc",
+    }
+
+    query_url = f"https://graph.microsoft.com/v1.0/users/{urllib.parse.quote(self.user_principal_name)}/calendars/{calendar['id']}/calendarView?{urllib.parse.urlencode(params)}"
+    print(f"Requesting calendar information for {calendar['name']}")
+    print("params",params)
+
+    querying = True
+    datas = []
+    while querying:
+      print("query_url:",query_url)
+      response = requests.get(query_url, headers=headers, timeout=1)
+      print(response.status_code)
+      print("")
+
+      data = response.json()
+      datas.append(data)
+      try:
+        query_url = data["@odata.nextLink"]
+      except KeyError:
+        querying = False
+
+    events = []
+    for data in datas:
+      for event in data["value"]:
+        events.append({
+          "subject": event["subject"],
+          "startDateTime": event["start"]["dateTime"],
+          "endDateTime": event["end"]["dateTime"]
+        })
+
+    return events
+
+  def get_events(self, startDateTime, endDateTime):
+    events = []
+    for calendar in self.calendars:
+      events += self.get_calendar_view(calendar, startDateTime, endDateTime)
+    return sorted(events, key=lambda event: event['startDateTime']) 
+
